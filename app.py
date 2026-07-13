4dfdef994086e2daea5c4cfb04914333f6ecc2de
Muhammad Hameez <hameez172@gmail.com>
2026-07-14 01:33:41 +0500

    Refactor app.py to improve structure and readability

--- app.py
+++ app.py
@@ -195,8 +195,8 @@ with tab2:
         
         c5, c6, c7 = st.columns(3)
         item_qty = c5.number_input("Qty", min_value=1.0, format="%g")
-        item_price = c6.number_input("Unit Price", min_value=0.0, format="%g")
-        item_cost = c7.number_input("Actual Cost", min_value=0.0, format="%g")
+        item_price = c6.number_input("Unit Price", min_value=0, format="%g")
+        item_cost = c7.number_input("Actual Cost", min_value=0, format="%g")
         
         paid = st.number_input("Payment sent by Client", min_value=0.0, format="%g")
 
